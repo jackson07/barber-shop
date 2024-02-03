0781a9a9a145aa7dbeb2ca3f1549602a1fb7f924
Jackson <jackson.borba@hotmail.com>
2024-02-03 11:24:50 -0300

    feat: add SideMenuComponent

--- app/barbershops/[id]/_components/barbershop-info.tsx
+++ app/barbershops/[id]/_components/barbershop-info.tsx
@@ -1,5 +1,7 @@
 "use client"
+import SideMenu from "@/app/_components/side-menu";
 import { Button } from "@/app/_components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/app/_components/ui/sheet";
 import { Barbershop } from "@prisma/client";
 import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react";
 import Image from "next/image";
@@ -10,9 +12,10 @@ interface BarberShopInfoProps {
 }
 
 const BarberShopInfo = ({barbershop}:BarberShopInfoProps) => {
-    const router = useRouter();   
+    const router = useRouter();  
+
     const handleBackClick = () => {
-        router.push("/")
+        router.back();
     }
 
     return (
@@ -25,10 +28,20 @@ const BarberShopInfo = ({barbershop}:BarberShopInfoProps) => {
                     onClick={handleBackClick}
                     >
                     <ChevronLeftIcon />
-                </Button>
-                <Button size="icon" variant="outline" className="z-50 absolute top-4 right-4">
-                    <MenuIcon />
-                </Button>
+                </Button>                
+
+                <Sheet>
+                    <SheetTrigger asChild>
+                        <Button size="icon" variant="outline" className="z-50 absolute top-4 right-4">
+                            <MenuIcon />
+                        </Button>
+                    </SheetTrigger>
+
+                    <SheetContent className="p-0">
+                        <SideMenu />
+                    </SheetContent>
+                </Sheet>
+
                 <Image
                     src={barbershop.imageUrl}
                     alt={barbershop.name}
