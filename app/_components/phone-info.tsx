f205172fdba05da1373470119e7777f1d9fce42f
Jackson <jackson.borba@hotmail.com>
2024-02-25 19:59:58 -0300

    fix: ajust gap on phone number card

--- app/_components/phone-info.tsx
+++ app/_components/phone-info.tsx
@@ -24,7 +24,7 @@ const PhoneInfo = ({phone}:PhoneInfoProps) => {
 
     return (        
         <div className="flex items-center justify-between my-3">
-            <div className="flex items-center gap-2 text-sm" ref={textRef}>
+            <div className="flex items-center gap-1 text-sm" ref={textRef}>
                 <Smartphone className="w-6 h-6" />
                 <p>{phone}</p>
             </div>
