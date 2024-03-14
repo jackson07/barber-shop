7e95fe3943da166120343912fb97c2518b2c3d01
Jackson <jackson.borba@hotmail.com>
2024-03-14 19:58:40 -0300

    fix: added confirmation msg on delete any service

--- app/barbershops/[id]/_components/service-delete.tsx
+++ app/barbershops/[id]/_components/service-delete.tsx
@@ -5,25 +5,44 @@ import useAuth from "@/app/_components/useAuth";
 import { Service } from "@prisma/client";
 import { Trash2 } from "lucide-react";
 import { deleteService } from "../_actions/delete-service";
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "@/app/_components/ui/alert-dialog";
 
 interface ServiceDeleteProps {
     service: Service
 }
 
-const ServiceDelete = ({service}:ServiceDeleteProps) => {
-    const { isAuthorized } = useAuth();   
+const ServiceDelete = ({ service }: ServiceDeleteProps) => {
+    const { isAuthorized } = useAuth();
     if (!isAuthorized) {
         return null;
     }
 
     const handleDeleteServiceClick = async () => {
-        await deleteService({serviceId: service.id});
+        await deleteService({ serviceId: service.id });
     }
 
     return (
-        <Button className="m-0 p-2" variant="ghost" onClick={handleDeleteServiceClick}>
-            <Trash2 size={20} className="text-destructive" />
-        </Button>
+        <AlertDialog>
+            <AlertDialogTrigger>
+                <Button className="m-0 p-2" variant="ghost">
+                    <Trash2 size={20} className="text-destructive" />
+                </Button>
+            </AlertDialogTrigger>
+            <AlertDialogContent className="w-[90%]">
+                <AlertDialogHeader>
+                    <AlertDialogTitle>Deseja mesmo excluir o serviço?</AlertDialogTitle>
+                    <AlertDialogDescription>
+                        Uma vez excluído, não será possível reverter essa ação. Porém seus agendamentos ficarão gravados.
+                    </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter className="flex-row gap-3">
+                    <AlertDialogCancel className="w-full gap-3 mt-0">Voltar</AlertDialogCancel>
+                    <AlertDialogAction onClick={handleDeleteServiceClick} className="w-full gap-3">
+                        Excluir
+                    </AlertDialogAction>
+                </AlertDialogFooter>
+            </AlertDialogContent>
+        </AlertDialog>
     );
 }
 
