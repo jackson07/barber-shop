5ac1aea2c46b3ea360ff68ac92f59283b8b8e147
Jackson <jackson.borba@hotmail.com>
2024-03-18 21:25:38 -0300

    fix: added loader to make charging when delete the service

--- app/barbershops/[id]/_components/service-delete.tsx
+++ app/barbershops/[id]/_components/service-delete.tsx
@@ -3,29 +3,37 @@
 import { Button } from "@/app/_components/ui/button";
 import useAuth from "@/app/_components/useAuth";
 import { Service } from "@prisma/client";
-import { Trash2 } from "lucide-react";
+import { Loader2, Trash2 } from "lucide-react";
 import { deleteService } from "../_actions/delete-service";
 import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "@/app/_components/ui/alert-dialog";
+import { useState } from "react";
+import { toast } from "sonner";
 
 interface ServiceDeleteProps {
     service: Service
 }
 
 const ServiceDelete = ({ service }: ServiceDeleteProps) => {
+    const [isLoading, setIsLoading] = useState(false);
+    
     const { isAuthorized } = useAuth();
     if (!isAuthorized) {
         return null;
     }
 
     const handleDeleteServiceClick = async () => {
+        setIsLoading(true);
         await deleteService({ serviceId: service.id });
+        toast("Serviço excluído com sucesso!");
+        setIsLoading(false);
     }
 
     return (
         <AlertDialog>
             <AlertDialogTrigger>
                 <Button className="m-0 p-2" variant="ghost">
-                    <Trash2 size={20} className="text-destructive" />
+                    {isLoading && <Loader2 className="h-4 w-4 animate-spin" />}
+                    {!isLoading &&  <Trash2 size={20} className="text-destructive" />}
                 </Button>
             </AlertDialogTrigger>
             <AlertDialogContent className="w-[90%]">
