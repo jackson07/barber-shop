9c256945bf62f984bb3b1f7ad2aa2fb1ed5f3904
Jackson <jackson.borba@hotmail.com>
2024-03-19 23:22:43 -0300

    fix: adjusted barbershop update to consider phone number

--- app/barbershops/[id]/_actions/update-barbershop.ts
+++ app/barbershops/[id]/_actions/update-barbershop.ts
@@ -3,22 +3,30 @@
 import { db } from "@/app/_lib/prisma";
 import { revalidatePath } from "next/cache";
 
-interface UpdateDescriptionBarbershopParams {
+interface UpdateBarbershopParams {
     formData: FormData
 }
 
-export const UpdateDescriptionBarbershop = async (params: UpdateDescriptionBarbershopParams) => {
+export const UpdateBarbershop = async (params: UpdateBarbershopParams) => {
     const id = params.formData.get("id") as string;
     const description = params.formData.get("description") as string;
+    const phone = params.formData.get("phone") as string;
 
-    await db.barbershop.update({
-        where: {
-            id: id,
-        },
-        data: {
-            description: description,
-        },
-    });
+    if (description !== null) {
+        await db.barbershop.update({
+            where: { id },
+            data: { description },
+        });
+    }
+
+    console.log("foneee", phone)
+
+    if (phone !== null) {
+        await db.barbershop.update({
+            where: { id },
+            data: { phone },
+        });
+    }
 
     revalidatePath("/");
     revalidatePath("/barbershops");
