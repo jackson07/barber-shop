b899a6a138864614c372907da734cf2f4a85ad73
Jackson <jackson.borba@hotmail.com>
2024-03-21 21:06:50 -0300

    fix: removing width max and added w-full to phone number component

--- app/_components/phone-info.tsx
+++ app/_components/phone-info.tsx
@@ -23,9 +23,9 @@ const PhoneInfo = ({phone}:PhoneInfoProps) => {
     if(!phone) return;    
 
     return (        
-        <div className="flex items-center justify-between my-3 max-w-[460px]">
+        <div className="flex items-center justify-between my-3 w-full">
             <div className="flex items-center gap-1 text-sm" ref={textRef}>
-                <Smartphone className="w-6 h-6" />
+                <Smartphone className="w-6 h-6"/>
                 <p>{phone}</p>
             </div>
             <Button variant="outline" className={copied ? "text-xs p-3" : ""} onClick={handleCopyClick}>{copied ? 'Copiado!' : 'Copiar'}</Button>
